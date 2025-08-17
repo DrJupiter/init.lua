0c21f7d7b5866432c827e407e1189c963cd9b1ca
Klaus Jupiter <klausjupiter@hotmail.com>
2025-08-17 09:44:11 +0200

    tex

--- lua/jupiter/lazy/vimtex.lua
+++ lua/jupiter/lazy/vimtex.lua
@@ -8,5 +8,17 @@ return {
     vim.g.vimtex_compiler_silent = 0
     vim.g.vimtex_quickfix_open_on_warning = 0
     vim.g.tex_conceal = 'abdmg'
+    vim.g.vimtex_compiler_latexmk_engines = { _ = '-xelatex' }
+    vim.g.vimtex_compiler_latexmk = {
+      -- Add compiler options here
+      options = {
+        -- "-pdf",
+        "-shell-escape",
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+      },
+    }
   end
 }
