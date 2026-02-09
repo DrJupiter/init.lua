3a5cd5734e5f0d442fa85ea1c96a9597d62f134f
Klaus Jupiter Bentzen <kjb@apeximaging.ai>
2026-02-09 17:45:11 +0000

    update treesitter to new api

--- lua/jupiter/lazy/treesitter.lua
+++ lua/jupiter/lazy/treesitter.lua
@@ -1,35 +1,37 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
+    config = function()
+        local treesitter = require("nvim-treesitter")
+        local languages = {
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "markdown",
+            "markdown_inline",
+            "latex",
+            "rust",
+            "html",
+            "javascript",
+            "python",
+            "typescript",
+            "svelte",
+            "c",
+            "cpp",
+        }
 
-        configs.setup({
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = {"lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "latex", "rust", "html", "javascript", "python", "typescript", "svelte", "c", "cpp"},
+        treesitter.setup({})
+        if #vim.api.nvim_list_uis() > 0 then
+            treesitter.install(languages)
+        end
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        local group = vim.api.nvim_create_augroup("jupiter_treesitter", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+            group = group,
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
         })
     end
 }
