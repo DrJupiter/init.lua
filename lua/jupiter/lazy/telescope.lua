321802acfdfc6910e309b6923479278e43c75ac1
Klaus Jupiter Bentzen <kjb@apeximaging.ai>
2026-02-24 15:05:43 +0000

    fixes

--- lua/jupiter/lazy/telescope.lua
+++ lua/jupiter/lazy/telescope.lua
@@ -1,11 +1,13 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()
-    require('telescope').setup({})
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find git files' })
-    vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end, { desc = 'Telescope find git files' })
-end
-    }
+    config = function()
+        require('telescope').setup({})
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find git files' })
+        vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end,
+            { desc = 'Telescope find git files' })
+    end
+}
