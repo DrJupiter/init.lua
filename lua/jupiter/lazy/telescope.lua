d2c253f46ea0f6ec4f9c3ab9af5070888e083c19
Klaus Jupiter Bentzen <kjb@apeximaging.ai>
2026-03-03 13:30:09 +0000

    add live grep

--- lua/jupiter/lazy/telescope.lua
+++ lua/jupiter/lazy/telescope.lua
@@ -1,13 +1,33 @@
 return {
     'nvim-telescope/telescope.nvim',
     version = '*',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-live-grep-args.nvim', version = '^1.0.0' },
+    },
     config = function()
-        require('telescope').setup({})
+        local telescope = require('telescope')
+        local lga_actions = require('telescope-live-grep-args.actions')
+        telescope.setup({
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true,
+                    mappings = {
+                        i = {
+                            ['<C-k>'] = lga_actions.quote_prompt(),
+                            ['<C-i>'] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
+                        },
+                    },
+                },
+            },
+        })
+        telescope.load_extension('live_grep_args')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find git files' })
-        vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end,
-            { desc = 'Telescope find git files' })
+        vim.keymap.set('n', '<leader>pg', function()
+            require('telescope').extensions.live_grep_args.live_grep_args({
+                additional_args = { '--hidden' },
+            })
+        end, { desc = 'Telescope live grep' })
     end
 }
